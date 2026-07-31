72c4e2d64112be90b69e4d95d421dbd246d2eea2
DELL <DELL@DESKTOP-N0ANFHP>
2026-07-31 13:14:22 +0530

    Tutorial 2

---
+++ pp13.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class pp13
+    {
+        static void Main(string[] args)
+        {
+            int amount;
+            Console.WriteLine("Enter Amount :");
+            amount = Convert.ToInt32(Console.ReadLine());
+
+            int[] notes = { 2000, 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+
+            foreach (int note in notes)
+            {
+                int count = amount / note;
+                Console.WriteLine("Notes of Rs. {0} = {1}", note, count);
+                amount = amount % note;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ pp14.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class pp14
+    {
+        static void Main(string[] args)
+        {
+            int maths, physics, chemistry, total = 0;
+            Console.WriteLine("Enter the marks obtained in Maths : ");
+            maths = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the marks obtained in Physics : ");
+            physics = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the marks obtained in Chemistry : ");
+            chemistry = Convert.ToInt32(Console.ReadLine());
+
+            total = maths + physics + chemistry;
+            int mathphy = maths + physics;
+
+            if((maths >= 65 && physics >=55 && chemistry>=50 && total >=180) || (maths >=65 &&  physics >=55 && mathphy >= 140))
+            {
+                Console.WriteLine("The candidate is eligible for Admission.");
+            }
+            else
+            {
+                Console.WriteLine("The candidate is Not eligible for Admission.");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
