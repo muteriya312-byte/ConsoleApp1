2dc7d3d6eb0a13ba362918f75cab55616ca5278d
DELL <DELL@DESKTOP-N0ANFHP>
2026-08-11 10:43:33 +0530

    tutorial - 3

---
+++ t6.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Line
+    {
+        private double length;
+
+        public Line()
+        {
+            Console.WriteLine("Object creating value length = 10");
+
+            length = 10;
+        }
+        public void setLength(double len)
+        {
+            length = len;
+        }
+        public double getLength()
+        {
+            return length;
+        }
+    }
+    class t6
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Line line = new Line();
+
+            // set line length
+
+            Console.WriteLine("Length of line : {0}", line.getLength());
+            line.setLength(6);
+            // set line length
+
+            Console.WriteLine("Length of line : {0}", line.getLength());
+
+            Console.ReadKey();
+
+            Console.WriteLine("Name : Mital Uteriya");
+            Console.WriteLine("Enrollment no. : 24SOECE11043");
+
+        }
+
+    }
+
+}
+
---
+++ t7.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class StudentDetails
+    {
+        private int enroll;
+        private string name;
+
+        public int e
+        {
+            get { return enroll; }
+            set { enroll = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set {  name = value; }
+        }
+
+        public StudentDetails()
+        {
+
+        }
+        public StudentDetails(int eno,string no)
+        {
+            enroll = eno;
+            Name = no;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Enrollment Number : " + enroll);
+            Console.WriteLine("Name : " + Name);
+        }
+
+    }
+    class t7
+    {
+        static void Main(string[] args)
+        {
+           StudentDetails student = new StudentDetails();
+
+            Console.WriteLine("Enter enrollment number : ");
+            student.e = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Name :");
+            student.Name = Console.ReadLine();
+
+            student.Display();
+
+            Console.WriteLine("Name : Mital Uteriya");
+            Console.WriteLine("Enrollment no. : 24SOECE11043");
+        }
+    }
+}
