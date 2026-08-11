79285685b8b287e6b2841f28b2641be174ab0e3b
DELL <DELL@DESKTOP-N0ANFHP>
2026-08-11 11:09:32 +0530

    tutorial-3

---
+++ t8.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class student
+    {
+        private int enrollmentNo;
+        private string name;
+        private string course;
+
+        public student(int eno , string nm, string crs)
+        {
+            enrollmentNo = eno;
+            name = nm;
+            course = crs;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("\nStudent Details");
+            Console.WriteLine("Enrollment No: " + enrollmentNo);
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("Course :" + course);
+        }
+    }
+
+    class t8
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Enrollment No. :");
+            int eno = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Name :");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Course:");
+            string course = Console.ReadLine();
+
+            student s = new student(eno, name, course);
+
+            s.Display();
+
+            Console.WriteLine("Enrollment No. : 24SOECE11043");
+            Console.WriteLine("Name : Mital Uteriya");
+            Console.ReadLine();
+        }
+            
+    }
+}
---
+++ t9.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class clock
+    {
+        private int hour;
+        private int minute;
+        private int second;
+
+        public clock()
+        {
+            hour = 12;
+            minute = 0;
+            second = 0;
+        }
+
+        public clock(int h , int m ,int s)
+        {
+            hour =h; minute=m; second = s;
+        }
+
+        public void Increment()
+        {
+            second++;
+            if (second == 60)
+            {
+                second = 0;
+                minute++;
+
+                if(minute == 60)
+                {
+                    minute = 0;
+                    hour++;
+
+                    if(hour == 24)
+                    {
+                        hour = 0;
+                    }
+                }
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine("Time : {0:D2} : {1:D2} : {2:D2}", hour, minute, second);
+        }
+    }
+    class t9
+    {
+        static void Main(string[] args)
+        {
+            clock c = new clock(10, 59, 59);
+
+            Console.WriteLine("Current Time");
+            c.Display();
+
+            c.Increment();
+
+            Console.WriteLine("After Increment");
+            c.Display();
+
+            c.Increment();
+
+            Console.WriteLine("Enrollment No. : 24SOECE11043");
+            Console.WriteLine("Name : Mital Uteriya");
+
+            Console.ReadLine();
+        }
+    }
+}
