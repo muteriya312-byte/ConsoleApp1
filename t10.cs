55a667e581b24caf1cee488d7a129c5e89e25277
muteriya312-byte <muteriya312@rku.ac.in>
2026-08-14 13:22:40 +0530

    tutorial-3

---
+++ t10.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class EmployeeDetails
+    {
+        private int empid;
+        private string empName;
+        private double basicSalary;
+        private double netSalary;
+
+        public EmployeeDetails(int id, string name, double salary)
+        {
+            empid = id;
+            empName = name;
+            basicSalary = salary;
+        }
+        public void CalculateSalary()
+        {
+            double hra = basicSalary * 0.20;
+            double da = basicSalary * 0.10;
+            netSalary = basicSalary + hra + da;
+        }
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine("-----------Employee Details--------------");
+            Console.WriteLine("Employee id is :" + empid);
+            Console.WriteLine("Employee name is :" + empName);
+            Console.WriteLine("Employee basic Salary is :" + basicSalary);
+            Console.WriteLine("Calculated net Salary is :" + netSalary);
+
+        }
+    }
+
+    class t10
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Employee Id ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Name :");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter basic Salary :");
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+            EmployeeDetails emp= new EmployeeDetails(id, name, salary);
+            emp.CalculateSalary();
+            emp.DisplayEmployeeDetails();
+
+            Console.WriteLine("Enrollment No. : 24SOECE11043");
+            Console.WriteLine("Name : Mital Uteriya");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ t11.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class BankAccout
+    {
+        private int accountNo;
+        private string accountHolder;
+        private double balance;
+
+        public BankAccout(int accNo, string holder , double ammount)
+        {
+            accountNo = accNo;
+            accountHolder = holder;
+            balance = ammount;
+        }
+
+        public void Deposit(double  amount)
+        {
+            balance += amount;
+            Console.WriteLine("Amoount Deposited Successfully. ");
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine("Amount Withdrawn successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Insufficent Balance.");
+            }
+        }
+        public void BalanceEnquiry()
+        {
+            Console.WriteLine("\n Account No :" + accountNo);
+            Console.WriteLine("Account Holder :" + accountHolder);
+            Console.WriteLine("Current Blanace :" + balance);
+        }
+    }
+
+    class t11
+    {
+        static void Main(string[] args)
+        {
+            BankAccout acc1 = new BankAccout(1001, "Mital", 50000);
+            BankAccout acc2 = new BankAccout(1002, "Hiral", 40000);
+
+            Console.WriteLine("Account 1 Transactions :");
+            acc1.Deposit(2000);
+            acc1.Withdraw(1000);
+            acc1.BalanceEnquiry();
+
+            Console.WriteLine("-----------------------");
+
+            Console.WriteLine("Account 2 Transactions :");
+            acc1.Deposit(1000);
+            acc1.Withdraw(2000);
+            acc1.BalanceEnquiry();
+
+
+            Console.WriteLine("Enrollment No. : 24SOECE11043");
+            Console.WriteLine("Name : Mital Uteriya");
+
+            Console.ReadLine();
+
+        }
+    }
+}
