aaadad2243d0d878ebcb2926ca6c684b05aff9dd
DELL <DELL@DESKTOP-N0ANFHP>
2026-08-21 13:40:55 +0530

    tutorial-3

--- t16.cs
+++ t16.cs
@@ -6,11 +6,64 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-   
+    class Employees1
+    {
+        public int EmployeeID { get; set; }
+        public string Name {  get; set; }
+        public double BasicSalary {  get; set; }
+
+        
+    }
+    class PermanentEmployee : Employees1
+    {
+        public void CalculateSalary()
+        {
+            double hra = BasicSalary * 0.20;
+            double da = BasicSalary * 0.10;
+            double netSalary = BasicSalary + hra + da;
+
+            Console.WriteLine("Permanent Employee");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Employee ID :" + EmployeeID);
+            Console.WriteLine("Name : "+Name);
+            Console.WriteLine("Basic Salary :" + BasicSalary);
+            Console.WriteLine("HRA :" + hra);
+            Console.WriteLine("DA :" + da);
+            Console.WriteLine("Net Salary :" + netSalary);
+        }
+    }
+    class ConstractEmployee : Employees1
+    {
+        public void CalculateSalary()
+        {
+            double netSalary = BasicSalary;
+
+            Console.WriteLine("Constract Employee");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Employee ID :" + EmployeeID);
+            Console.WriteLine("Name : " + Name);
+            Console.WriteLine("Salary :" + netSalary);
+
+        }
+    }
     internal class t16
     {
         static void Main(string[] args)
         {
+            PermanentEmployee p = new PermanentEmployee();
+
+            p.EmployeeID = 101;
+            p.Name = "Mital";
+            p.BasicSalary = 30000;
+            p.CalculateSalary();
+
+            ConstractEmployee c = new ConstractEmployee();
+
+            c.EmployeeID = 102;
+            c.Name = "Hiral";
+            c.BasicSalary = 25000;
+            c.CalculateSalary() ;
+
             Console.WriteLine("Name : Mital Uteriya");
             Console.WriteLine("Enrollment No. : 24SOECE11043");   
 
---
+++ t17.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //virtual will allowed to change method/override in child classes
+    //override keyword written in the child class's method.
+   //virtual keyword written in the parents class's method.
+    class Shape
+    {
+        public virtual void CalculatedArea()
+        {
+            Console.WriteLine("Area of Shape");
+        }
+    }
+    class Circle : Shape
+    {
+        public override void CalculatedArea()
+        {
+            double radius = 5;
+            double area = Math.PI * radius * radius;
+
+            Console.WriteLine("Area of Circle = " + area.ToString("0.00"));
+        }
+    }
+    class Rectangle : Shape
+    {
+        public override void CalculatedArea()
+        {
+            double length = 10;
+            double width = 5;
+
+            double area = length * width;
+
+            Console.WriteLine("Area of Rectangle = " + area);
+        }
+    }
+    class Triangle : Shape
+    {
+        public override void CalculatedArea()
+        {
+            double b = 10;
+            double h = 5;
+
+            double area = 0.5 * b*h;
+            Console.WriteLine("Area of Triangle = " + area);
+        }
+    }
+    internal class t17
+    {
+        static void Main(string[] args)
+        {
+            Shape shape;
+
+            //circle
+            shape = new Circle();
+            shape.CalculatedArea();
+
+            //rectangle
+            shape = new Rectangle();
+            shape.CalculatedArea();
+
+            //triangle
+            shape = new Triangle();
+            shape.CalculatedArea();
+
+            Console.WriteLine("Name : Mital Uteriya");
+            Console.WriteLine("Enrollment No. : 24SOECE11043");
+
+            Console.ReadLine();
+        }
+    }
+}
